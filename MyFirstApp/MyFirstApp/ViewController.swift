40cf09d8aa09bcdedf564ead80e09af8b143b983
Hao Wu <tohaowu@gmail.com>
2015-09-17 13:35:29 -0700

    still so many files

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Hao Wu on 9/15/15.
+//  Copyright © 2015 Hao Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
